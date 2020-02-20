fc9ff8c74a8c33e4d2ac4e49f369fc116983b7d2
Jurgen Voorneveld <jegvoorneveld@gmail.com>
2020-02-20 20:57:17 +0100

    Initial commit

---
+++ src/main/java/keepass/BobPlugin.java
@@ -0,0 +1,45 @@
+package keepass;
+
+import bobthebuildtool.pojos.buildfile.Project;
+import bobthebuildtool.pojos.error.InvalidInput;
+import de.slackspace.openkeepass.KeePassDatabase;
+import de.slackspace.openkeepass.domain.KeePassFile;
+import jcli.errors.InvalidCommandLine;
+
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Map;
+
+import static bobthebuildtool.services.Functions.isNullOrEmpty;
+import static java.nio.file.Files.isRegularFile;
+import static jcli.CliParserBuilder.newCliParser;
+
+public enum BobPlugin {;
+
+    public static void installPlugin(final Project project) {
+        project.addCommand("keepass", "Loads variables from a keepass archive", BobPlugin::loadKeepassArchive);
+    }
+
+    private static int loadKeepassArchive(final Project project, final Map<String, String> env, final String[] args)
+            throws InvalidCommandLine, InvalidInput {
+        final CliKeepass arguments = newCliParser(CliKeepass::new).parse(args);
+
+        final Path archive = Paths.get(arguments.file);
+        if (!isRegularFile(archive)) throw new InvalidInput("Keepass archive " + archive.toAbsolutePath() + " does not exist.");
+
+        final String password = isNullOrEmpty(arguments.password) ? env.get("KEEPASS_MASTER_PASSWORD") : arguments.password;
+        if (isNullOrEmpty(password)) throw new InvalidInput("Could not find a password to use for the keepass archive");
+
+        final KeePassFile database = KeePassDatabase
+            .getInstance(archive.toFile())
+            .openDatabase(password);
+
+        for (final var entry : database.getEntries()) {
+            System.out.println("Title: " + entry.getTitle() + ", Username: " + entry.getUsername() + ", Password: " + entry.getPassword());
+            env.put(entry.getUsername(), entry.getPassword());
+        }
+
+        return 0;
+    }
+
+}
---
+++ src/main/java/keepass/CliKeepass.java
@@ -0,0 +1,12 @@
+package keepass;
+
+import jcli.annotations.CliOption;
+
+public class CliKeepass {
+
+    @CliOption(name = 'f', longName = "file", isMandatory = true)
+    public String file;
+    @CliOption(name = 'p', longName = "password")
+    public String password;
+
+}
